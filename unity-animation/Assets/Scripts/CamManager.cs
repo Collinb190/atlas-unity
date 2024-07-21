1b7fefa66b23ae0db77beedabbd1f8e59f5f106d
Lin <100497279+Collinb190@users.noreply.github.com>
2024-07-21 12:00:29 -0500

    ty on board

--- unity-animation/Assets/Scripts/CamManager.cs
+++ unity-animation/Assets/Scripts/CamManager.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class CameraSwitcher : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject timer;
+    public GameObject dummy;
     public GameObject custsceneObject;
     public Animator cutsceneAnimator;   // Reference to the Animator component
     public float animationSpeed = 0.25f; // Speed multiplier for the animation
 
     void Start()
     {
+        player.SetActive(false);
+        timer.SetActive(false);
         // Set the speed of the animation
         cutsceneAnimator.speed = animationSpeed;
     }
@@ -15,5 +20,8 @@ public class CameraSwitcher : MonoBehaviour
     public void SwitchToMainCamera()
     {
         custsceneObject.SetActive(false);
+        timer.SetActive(true);
+        player.SetActive(true);
+        dummy.SetActive(false);
     }
 }
